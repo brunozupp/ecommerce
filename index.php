60a8a4bdfbcef88e2a70113e57fb2c686daf9832
Bruno Noveli <brunimzuppwolf@gmail.com>
2019-10-13 17:06:33 -0300

    Alterando para igual o curso

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php  
 
-	echo "Oiiii";
+	require_once("vendor/autoload.php");
+
+	$app = new \Slim\Slim();
+
+	$app->config('debug', true);
+
+	$app->get('/', function() {
+		echo "OK";
+	});
+
+	$app->run();
 
 ?>
