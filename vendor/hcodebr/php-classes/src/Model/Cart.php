1bd5426bbb972ed4883e108cf1d6da87e531cdea
Bruno Noveli <brunimzuppwolf@gmail.com>
2019-10-25 20:12:38 -0300

    Adicionar no carrinho

--- vendor/hcodebr/php-classes/src/Model/Cart.php
+++ vendor/hcodebr/php-classes/src/Model/Cart.php
@@ -6,6 +6,7 @@
 	use \Hcode\Model;
 	use \Hcode\Mailer;
 	use \Hcode\Model\User;
+	use \Hcode\Model\Product;
 
 	// No caso, vou gravar o id do carrinho numa sessão
 	class Cart extends Model {
@@ -96,6 +97,55 @@
 
 			$this->setData($results[0]);
 
+		}
+
+		public function addProduct(Product $product) {
+
+			$sql = new Sql();
+
+			$sql->query("INSERT INTO tb_cartsproducts(idcart, idproduct) VALUES(:idcart, :idproduct)", [
+				':idcart' => $this->getidcart(),
+				':idproduct' => $product->getidproduct()
+			]);
+		}
+
+		public function removeProduct(Product $product, $all = false) {
+
+			$sql = new Sql();
+
+			if($all) {
+
+				// Removendo todos os produtos (não importa a quantidade que colocou dele)
+				$sql->query("UPDATE tb_cartsproducts SET dtremoved = NOW() WHERE idcart = :idcart AND idproduct = :idproduct AND dtremoved IS NULL", [
+					'idcart' => $this->getidcart(),
+					'idproduct' => $product->getidproduct()
+				]);
+			} else {
+
+				// Remove apenas uma unidade do produto
+				$sql->query("UPDATE tb_cartsproducts SET dtremoved = NOW() WHERE idcart = :idcart AND idproduct = :idproduct AND dtremoved IS NULL LIMIT 1", [
+					'idcart' => $this->getidcart(),
+					'idproduct' => $product->getidproduct()
+				]);
+			}
+		}
+
+		public function getProducts() {
+
+			$sql = new Sql();
+
+			$rows = $sql->select("
+				SELECT b.idproduct, b.desproduct, b.vlprice, b.vlwidth, b.vlheight, b.vllength, b.vlweight, b.desurl, COUNT(*) AS nrqtd, SUM(b.vlprice) AS vltotal
+				FROM tb_cartsproducts a
+				INNER JOIN tb_products b ON a.idproduct = b.idproduct
+				WHERE a.idcart = :idcart AND a.dtremoved IS NULL
+				GROUP BY b.idproduct, b.desproduct, b.vlprice, b.vlwidth, b.vlheight, b.vllength, b.vlweight, b.desurl
+				ORDER BY b.desproduct
+			", [
+				':idcart' => $this->getidcart()
+			]);
+
+			return Product::checkList($rows);
 		}
 	}
 
