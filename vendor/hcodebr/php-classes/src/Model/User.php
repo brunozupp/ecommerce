048e88d1aa9a5ee3333f8e28e971a87144bcb8e3
Bruno Noveli <brunimzuppwolf@gmail.com>
2019-10-27 14:38:07 -0300

    Esqueceu senha site

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -170,7 +170,7 @@
 			));
 		}
 
-		public static function getForgot($email) {
+		public static function getForgot($email, $inadmin = true) {
 
 			$sql = new Sql();
 
@@ -203,8 +203,11 @@
 
 					$code = base64_encode($code);
 
+					if($inadmin === true) {
 						$link = "http://www.hcodecommerce.com.br/admin/forgot/reset?code=$code";
-						//$link = "http://www.hcodecommerce.com.br/forgot/reset?code=$code";
+					} else {
+						$link = "http://www.hcodecommerce.com.br/forgot/reset?code=$code";
+					}
 
 					$mailer = new Mailer($data["desemail"], $data["desperson"], "Redefinir Senha da Hcode Store", 
 						"forgot", array(
